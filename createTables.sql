bab44246bfbc47e01d01e754620b74505d2a1368
green1490 <zambokrisztian1490@gmail.com>
2023-07-10 11:57:26 +0200

    finish db

--- createTables.sql
+++ createTables.sql
@@ -1,6 +1,32 @@
-CREATE TABLE users (
-	id serial PRIMARY KEY,
-	username VARCHAR ( 10 ) UNIQUE NOT NULL,
-	password VARCHAR ( 12 ) NOT NULL,
-	email VARCHAR ( 30 ) UNIQUE NOT NULL
+CREATE TABLE Comments (
+	id			serial 	PRIMARY KEY,
+	parentID	integer,
+	userText	text NOT NULL,
+	deleted		boolean NOT NULL
+);
+
+CREATE TABLE Users (
+	id 			serial PRIMARY KEY,
+	email 		VARCHAR ( 30 ) UNIQUE NOT NULL,
+	username 	VARCHAR ( 10 ) UNIQUE NOT NULL,
+	password 	VARCHAR ( 12 ) NOT NULL,
+	karma 		integer DEFAULT 0
+);
+
+CREATE TABLE UsersComments (
+	id			serial PRIMARY KEY,
+	commentID	integer REFERENCES Comments(id),
+	userID		integer REFERENCES Users(id)
+);
+
+CREATE TABLE Thread (
+	id 			serial PRIMARY KEY,
+	userID	 	integer REFERENCES Users(id),
+	commentsID	integer REFERENCES UsersComments(id),
+	deleted		boolean NOT NULL
+);
+
+CREATE TABLE ThreadCol (
+	id			serial PRIMARY KEY,
+	threadID	integer REFERENCES Thread(id)
 );
